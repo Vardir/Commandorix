f22b597f597c6b9bf1041bd63cff8159afe06f98
Vardir <andriy.satura@gmail.com>
2022-01-09 17:18:21 +0200

    feature(utils): add enum helper class

---
+++ sources/Utils.Tests/Helpers/EnumHelperTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+using FluentAssertions;
+
+using Vardirsoft.Commandorix.Utils.Helpers;
+
+using Xunit;
+
+namespace Utils.Tests.Helpers;
+
+public class EnumHelperTests
+{
+  [Fact]
+  public void GetAllValues_OnEnum_ReturnsAllDeclaredEnumValues()
+  {
+    var values = EnumHelper.GetAllValues<FixedEnum>().ToArray();
+
+    values.Should().HaveCount(3);
+    values.Should().ContainSingle(e => e == FixedEnum.Value1);
+    values.Should().ContainSingle(e => e == FixedEnum.Value2);
+    values.Should().ContainSingle(e => e == FixedEnum.Value3);
+  }
+}
+
+public enum FixedEnum
+{
+  Value1,
+  Value2,
+  Value3
+}
---
+++ sources/Utils/Helpers/EnumHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Vardirsoft.Commandorix.Utils.Helpers;
+
+public static class EnumHelper
+{
+  public static IEnumerable<T> GetAllValues<T>() where T : Enum
+  {
+    var type = typeof(T);
+    var fields = type.GetFields(BindingFlags.Static | BindingFlags.Public);
+
+    return fields.Select(f => f.GetValue(null)).OfType<T>();
+  }
+}
